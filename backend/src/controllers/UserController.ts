b1132361919333cefc29278f1d4fe44b71b086e4
ccastro01 <alvescastro00cca13@gmail.com>
2020-12-25 23:34:36 -0300

    Criando uma nova rota para envio de email

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from 'express'
+import EmailService from '../services/EmailService';
 
 const users = [{ name: "Chris", email: "christian.alves@dellead.com" }];
 
 export default{
     async index(req: Request, res: Response){
         return res.json(users)
+    },
+    
+    async create(req: Request, res: Response){
+        const emailService = new EmailService()
+
+        emailService.sendMail(
+            { name: "Christian Castro", email: "christian.alves@dellead.com" },
+            { subject: 'Bem-vindo ao sistema', body: 'Seja bem-vindo'}
+        );
+
+        return res.send();
     }
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,5 +4,6 @@ import UserController from './controllers/UserController'
 const routes = Router()
 
 routes.get('/users', UserController.index)
+routes.get('/users/create', UserController.create)
 
 export default routes
