5e72ef19fcea6ea66032a494d3758500d5e84eba
ccastro01 <alvescastro00cca13@gmail.com>
2020-12-26 18:51:27 -0300

    Tipagens de parâmetros

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
+
+interface IUser {
+  name: string;
+  email: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    api.get<IUser[]>("/users").then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-  
+      {users.map((user) => (
+        <p>{user.name}</p>
+      ))}
     </div>
   );
 }
